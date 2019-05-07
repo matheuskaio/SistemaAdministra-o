47cacb4fbe3f401f94211e16d4c55d57d833d659
Márcio Douglas Xavier <marcio.xavier@academico.ifrn.edu.br>
2019-05-07 17:32:34 -0300

    Class CRUD

--- domain/dbaccess/connection.php
+++ domain/dbaccess/connection.php
@@ -1,10 +1,10 @@
-<?php  
+<?php
 	$host = '162.241.203.36';
 	$user = 'msdeve73_admin2';
 	$password = '12345';
 	$dbname = 'msdeve73_karate';
-	$conn = new mysqli($host, $user, $password,$dbname);
+	public $conn = new mysqli($host, $user, $password,$dbname);
 	if ($conn->connect_error) {
 	    die("Connection failed: " . $conn->connect_error);
-	} 
+	}
 ?>
--- domain/dbaccess/crud.php
+++ domain/dbaccess/crud.php
@@ -1,4 +1,43 @@
 <?php
     include 'select.php';
     include 'update.php';
+    public class CRUD{
+      function insert($table,$obj){
+          $sql = 'insert into '.$table.' (';
+          foreach ($obj as $key => $value) {
+            $sql .= $key.',';
+          }
+          $sql = substr_replace($sql,') values (', -1);
+          foreach ($obj as $value) {
+            $sql .= $value.',';
+          }
+          $sql = substr_replace($sql,')', -1);
+          echo $sql;
+      }
+      function deletar($table,$condiction){
+        echo 'delete from '.$table.' where '.$condiction;
+      }
+
+      function update($table,$obj,$condiction){
+        $sql = 'update '.$table;
+        foreach ($obj as $key => $value) {
+          $sql .= 'set '.$key.'='.$value.',';
+        }
+        $sql = substr_replace($sql,'', -1);
+        echo $sql.' where '.$condiction;
+      }
+
+      function select($fields,$condiction){
+        $sql = 'select ';
+        foreach ($fields as $key => $value) {
+          $sql.=$value.',';
+        }
+        $sql = substr_replace($sql,' from ', -1);
+        echo $sql.=$condiction;
+      }
+    }
+    // insert('teste',array('id'=>'marcio','nome'=>'marcio','livro'=>'book'));
+    // select(['id','nome'],'teste where   id = 1');
+    // deletar('teste','id = 1');
+    // update('teste',array('nome'=>'marcio','livro'=>'book'),'id=1')
 ?>
