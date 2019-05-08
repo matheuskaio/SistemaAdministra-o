a6cb432a01d78468d7e4440d287b2ee73bf2bbf9
Márcio Douglas <marciodouglasxavier@gmail.com>
2019-05-07 23:01:56 -0300

    Configurando URL amigavel

--- views/components/nave.php
+++ views/components/nave.php
@@ -1,15 +1,15 @@
 <div class="menuficadento">
 	<nav class="navegacao">
 		<ul class="menu">
-			<li><a href="?pagina=home" class="b1">Início</a></li>
-				    
+			<li><a href="home" class="b1">Início</a></li>
+
 			<li>
 				<a href="" class="b2">Gerenciar Alunos</a>
 				<ul class="submenu">
-				    <li><a href="?pagina=cadastrar">Cadastrar</a></li>
-				    <li><a href="?pagina=alterar">Alterar</a></li>
-				    <li><a href="?pagina=trancar">Trancar Matrícula</a></li>
-				    <li><a href="?pagina=reativar">Reativar Matrícula</a></li>
+				    <li><a href="cadastrar">Cadastrar</a></li>
+				    <li><a href="alterar">Alterar</a></li>
+				    <li><a href="trancar">Trancar Matrícula</a></li>
+				    <li><a href="reativar">Reativar Matrícula</a></li>
 				</ul>
 			</li>
 
@@ -26,10 +26,10 @@
 			<li>
 				<a href="" class="b2">Gerenciar Finanças</a>
 				<ul class="submenu">
-				    <li><a href="?pagina=relatorios">Gerar Relatórios</a></li>
-				    <li><a href="?pagina=mensalidade">Definir Mensalidade</a></li>
-				    <li><a href="?pagina=pendencias">Ver Pendências</a></li>
-					<li><a href="?pagina=descontos">Definir Descontos</a></li>
+				    <li><a href="relatorios">Gerar Relatórios</a></li>
+				    <li><a href="mensalidade">Definir Mensalidade</a></li>
+				    <li><a href="pendencias">Ver Pendências</a></li>
+					<li><a href="descontos">Definir Descontos</a></li>
 				</ul>
 			</li>
 
